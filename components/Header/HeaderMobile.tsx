c8abc76efd0689635e8acf067c34142736230197
azharalifauzi <azharalifauzi@gmail.com>
2022-03-25 19:45:28 +0700

    finish header responsive

--- components/Header/HeaderMobile.tsx
+++ components/Header/HeaderMobile.tsx
@@ -1,15 +1,46 @@
-import { Box, chakra, Icon, VStack } from '@chakra-ui/react';
+import {
+  Box,
+  chakra,
+  Flex,
+  Icon,
+  Text,
+  useDisclosure,
+  useOutsideClick,
+  VStack
+} from '@chakra-ui/react';
 import { LogoMarv } from 'components';
 import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdOutlineKeyboardArrowDown } from 'react-icons/md';
 import Link from 'next/link';
 import { motion, AnimatePresence } from 'framer-motion';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import Router from 'next/router';
 
 const HeaderMobile = () => {
   const [isOpen, setOpen] = useState<boolean>(false);
+  const { isOpen: isShow, onToggle, onClose } = useDisclosure();
+  const headerRef = useRef(null);
+
+  useOutsideClick({
+    ref: headerRef,
+    handler: () => {
+      setOpen(false);
+    }
+  });
+
+  useEffect(() => {
+    const handler = () => {
+      setOpen(false);
+      onClose();
+    };
+    Router.events.on('routeChangeComplete', handler);
+
+    return () => Router.events.off('routeChangeComplete', handler);
+  }, []);
 
   return (
     <Box
+      ref={headerRef}
       position="fixed"
       top="0"
       left="0"
@@ -55,14 +86,45 @@ const HeaderMobile = () => {
             <VStack
               fontFamily="aliencow"
               color="brand.secondary"
-              justifyContent="center"
               h="100%"
               fontSize="2xl"
+              pt="8"
+              alignItems="flex-start"
+              px="12"
             >
-              <Box>BERANDA</Box>
-              <Box>JASA KAMI</Box>
-              <Box>PORTFOLIO</Box>
-              <Box>KONTAK</Box>
+              <Link href="/" passHref>
+                <Box as="a">BERANDA</Box>
+              </Link>
+              <Flex alignItems="center" onClick={onToggle}>
+                <Text mr="2">JASA KAMI</Text>
+                <Icon
+                  as={MdOutlineKeyboardArrowDown}
+                  transform={isShow ? 'rotate(180deg)' : 'rotate(0deg)'}
+                  transition="all 0.2s"
+                />
+              </Flex>
+              {isShow && (
+                <>
+                  <Link href="/desain-interior">
+                    <a>DESAIN INTERIOR</a>
+                  </Link>
+                  <Link href="/desain-arsitek">
+                    <a>DESAIN ARSITEK</a>
+                  </Link>
+                  <Link href="/jasa-pembangunan">
+                    <a>JASA PEMBANGUNAN</a>
+                  </Link>
+                  <Link href="/custom-furniture">
+                    <a>KUSTOM FURNITURE</a>
+                  </Link>
+                </>
+              )}
+              <Link href="/portfolio" passHref>
+                <Box as="a">PORTFOLIO</Box>
+              </Link>
+              <Link href="/kontak" passHref>
+                <Box as="a">KONTAK</Box>
+              </Link>
             </VStack>
           </Box>
         )}
